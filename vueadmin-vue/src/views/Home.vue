9240632158416a307cee58fbf4c046d74c581ab6
Eascaty <63639459+Eascaty@users.noreply.github.com>
2021-09-08 15:28:08 +0800

    安装element-ui

---
+++ vueadmin-vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+
+
+
+    <el-button>测试</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+
+}
+</script>
