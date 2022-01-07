e0e250cd9eb1e549ca45a645b6b590259bb008e3
Eascaty <510252916@qq.com>
2022-01-07 16:33:03 +0800

    项目完成

--- vueadmin-java/src/main/java/com/Eascaty/common/lang/Result.java
+++ vueadmin-java/src/main/java/com/Eascaty/common/lang/Result.java
@@ -15,7 +15,6 @@ public class Result implements Serializable {
     return succ(200,"操作成功",data);
     }
 
-
     public static Result succ(int code, String msg, Object data) {
         Result r =  new Result();
         r.setCode(200);
@@ -38,5 +37,6 @@ public class Result implements Serializable {
     }
 
 
+//    项目完成
 
 }
